8cf1e44ee8c4343013787d5b6ccc1b89be5e2e35
SierraDavis2000 <99284604+SierraDavis2000@users.noreply.github.com>
2022-03-31 16:25:04 -0400

    Add files via upload

--- search.js
+++ search.js
@@ -4,10 +4,30 @@ var getListings = function() {
     fetch("");
   }
 
-  //search bar/btn
+
 fetch("").then(function(response) {
     response.json().then(function(data) {
       console.log(data);
     });
   })
 
+//search form input
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#form-control");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+  }
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+// get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
