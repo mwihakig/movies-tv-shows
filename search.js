793470cd3ef09b5c5941d89a37e66415414f2333
SierraDavis2000 <99284604+SierraDavis2000@users.noreply.github.com>
2022-03-31 16:23:38 -0400

    Add files via upload

---
+++ search.js
@@ -0,0 +1,13 @@
+
+// function to call movie/tv show API
+var getListings = function() {
+    fetch("");
+  }
+
+  //search bar/btn
+fetch("").then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  })
+
